d68c68168ecd0a4cdf5c72167460143080c21bfa
Kris <kristiandixon1206@gmail.com>
2025-05-08 20:13:01 +0100

    Commit to switch back to laptop. Just about there but issue with tbn

--- src/demos/steep_parallax/shaders/steep_parallax.vs
+++ src/demos/steep_parallax/shaders/steep_parallax.vs
@@ -11,20 +11,20 @@ attribute vec4 tangent;
 void main()	{
 
     vUv = uv;
-    gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0 );
+    gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
 
     //Move worldspace info to local space
     vec3 osEyePos = (InvModelMatrix * vec4(cameraPosition,1.0)).xyz;
     vec3 osLightPos = (InvModelMatrix * vec4(LightPos,1.0)).xyz;
 
-    mat4 tbn = mat4()
     //Move to tangent space
     vec3 binormal = normalize(cross(normal, tangent.xyz) * tangent.w);
-    mat3 tbn = mat3(vTangent, vBinormal, vNormal);
+    mat3 tbn = mat3(tangent.xyz, binormal, normal);
     
-    vec3 eyeDir = normalize(osEyePos - posiiton);
+    vec3 eyeDir = normalize(osEyePos - position);
     vTSEyeDir = tbn * eyeDir;
+    vTSEyeDir = eyeDir;
 
-    vec3 lightDir = normalize(osLightPos - posiiton);
+    vec3 lightDir = normalize(osLightPos - position);
     vTSLightDir = tbn * lightDir;
 }
--- src/demos/steep_parallax/steep_parallax.ts
+++ src/demos/steep_parallax/steep_parallax.ts
@@ -3,15 +3,24 @@ import DemoBase from "../../SceneBase";
 import { OrbitalCamera } from "../../shared/generic_scene_elements/camera";
 import { DefaultLighting } from "../../shared/generic_scene_elements/lighting";
 
+import albedoTex from "./textures/Rock062_1K-PNG_Color.png"
+import normalTex from "./textures/Rock062_1K-GL-PackedDisp.png"
+
+import vertexShader from "./shaders/steep_parallax.vs"
+import fragmentShader from "./shaders/steep_parallax.fs"
+
 export class SteepParallaxDemo extends DemoBase
 {
     camera: THREE.Camera;
     scene: THREE.Scene;
 
     materialUniforms = {
-        albedoMap: {value:null},
-        normalMap: {value:null},
-        bumpScale: {value:0.05}
+        AlbedoMap: {value:null},
+        NormalMap: {value:null},
+        BumpScale: {value:0.05},
+        InvModelMatrix: {value:null},
+        LightPos:{value:new THREE.Vector3(0,10,-5)}
+        
     }
 
     initialize(options?: any) {
@@ -19,12 +28,32 @@ export class SteepParallaxDemo extends DemoBase
         this.scene = new THREE.Scene();
         this.camera = new OrbitalCamera(70,0.001,10.0,this.renderer);
 
-        let mesh = new THREE.Mesh(quad, new THREE.MeshStandardMaterial());
+        let mesh = new THREE.Mesh(quad, new THREE.ShaderMaterial(
+            {
+                vertexShader:vertexShader,
+                fragmentShader:fragmentShader,
+                uniforms:this.materialUniforms
+            }
+        ));
         mesh.position.setZ(-1.0);
         this.scene.add(mesh);
 
+        let invMatrix = new THREE.Matrix4().copy(mesh.matrixWorld).invert();
+        this.materialUniforms.InvModelMatrix.value = invMatrix;
+
         DefaultLighting.SetupDefaultLighting(this.scene);
         this.scene.background = new THREE.Color("Green");
+
+        let textureLoader = new THREE.TextureLoader();
+        textureLoader.load(albedoTex, (tex)=>{
+            tex.wrapS = tex.wrapT = THREE.RepeatWrapping;
+            this.materialUniforms.AlbedoMap.value = tex;
+        });
+
+        textureLoader.load(normalTex, (tex)=>{
+            tex.wrapS = tex.wrapT = THREE.RepeatWrapping;
+            this.materialUniforms.NormalMap.value = tex;
+        });
     }
 
     override update(options?: any): void {
